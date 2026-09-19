ced9b39475a8f259ca33165b2ff768cbd1a897cb
EmanuelAlves <eaferreira@allianceconsultoria.com.br>
2026-09-18 22:07:59 -0300

    Adiciona lógica de roteamento e vinculação de dados na visualização de voos

--- fullscreen/webapp/controller/Flights.controller.js
+++ fullscreen/webapp/controller/Flights.controller.js
@@ -1,10 +1,52 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
     "use strict";
 
-    return Controller.extend("student00i.com.sap.training.ux402.fullscreen.fullscreen.controller.Flights", {
-        onInit() {
-        }
-    });
+    return Controller.extend(
+        "student00i.com.sap.training.ux402.fullscreen.fullscreen.controller.Flights",
+        {
+            onInit() {
+                var oRouter = this.getRouter();
+
+                oRouter.getRoute("flights").attachMatched(this._onObjectMatched, this);
+            },
+            getRouter: function () {
+                return sap.ui.core.UIComponent.getRouterFor(this);
+            },
+            _onObjectMatched: function (oEvent) {
+                var oArgs = oEvent.getParameter("arguments");
+                this._sCarrierId = oArgs.carrid;
+                var oView = this.getView();
+
+                oView.bindElement({
+                    path: "/UX_C_Carrier_TP('" + this._sCarrierId + "')",
+                    events: {
+                        change: this._onBindingChange.bind(this),
+                        dataRequested: function () {
+                            oView.setBusy(true);
+                        },
+                        dataReceived: function () {
+                            oView.setBusy(false);
+                        }
+                    }
+                });
+            },
+            _onBindingChange: function () {
+                var oElementbinding = this.getView().getElementBinding();
+
+                if (oElementbinding && oElementbinding.getBoundContext() === null) {
+                    this.getRouter().getTargets().display("notFound");
+                }
+            },
+            onNavBack: function () {
+                var oHistory = sap.ui.core.routing.History.getInstance();
+                var sPreviousHash = oHistory.getPreviousHash();
+
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    this.getRouter().navTo("overview", {}, true);
+                }
+            }            
+        },
+    );
 });
