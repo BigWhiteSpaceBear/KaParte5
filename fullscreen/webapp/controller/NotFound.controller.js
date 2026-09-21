e16f3faa362332eeb60a280fa2385149a62952e1
EmanuelAlves <eaferreira@allianceconsultoria.com.br>
2026-09-21 18:44:17 -0300

    Implementa navegação e exibe mensagem de erro na visualização de NotFound; ajusta colunas na visualização de Flights e Carrier

--- fullscreen/webapp/controller/NotFound.controller.js
+++ fullscreen/webapp/controller/NotFound.controller.js
@@ -4,7 +4,20 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("student00i.com.sap.training.ux402.fullscreen.fullscreen.controller.NotFound", {
-        onInit() {
+
+        getRouter: function () {
+            return sap.ui.core.UIComponent.getRouterFor(this);
+        },
+        
+        onNavBack: function () {
+            var oHistory = sap.ui.core.routing.History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                this.getRouter().navTo("overview", {}, true);
+            }
         }
     });
 });
