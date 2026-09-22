4226694496deabcc172109c75741fb9df93477e8
EmanuelAlves <eaferreira@allianceconsultoria.com.br>
2026-09-22 18:28:03 -0300

    Adiciona funcionalidade de reserva de voos com confirmação e tratamento de erros; atualiza a visualização de voos para incluir botão de reserva e novos textos de i18n.

--- fullscreen/webapp/controller/Flights.controller.js
+++ fullscreen/webapp/controller/Flights.controller.js
@@ -1,4 +1,8 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
+], (Controller, MessageToast, MessageBox) => {
     "use strict";
 
     return Controller.extend(
@@ -46,7 +50,58 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
                 } else {
                     this.getRouter().navTo("overview", {}, true);
                 }
-            }            
-        },
+            },
+            onHoverPress: function (oEvent) {
+                var carrid = oEvent.getSource().data("id");
+                var fldate = oEvent.getSource().data("fldate");
+                var connid = oEvent.getSource().data("connid");
+
+                MessageBox.confirm("Are you sure you want to book this flight?", function (oAction) {
+                    if (oAction === MessageBox.Action.OK) {
+                        var oEntry = {};
+                        oEntry.Carrid = carrid;
+                        oEntry.Fldate = fldate;
+                        oEntry.Connid = connid;
+                        oEntry.Counter = "1";
+                        oEntry.Bookid = "43535432";
+                        oEntry.Customid = "00003406";
+                        oEntry.Passname = "John Doe";
+
+
+                    }
+                    var oModel = this.getOwnerComponent().getModel();
+                    var customerHeader = { "Content-Type": "application/json" };
+                    oModel.setHeaders(customerHeader);
+
+                }.bind(this));
+
+            },
+
+            _createBookingEntry: function (oModel, oEntry) {
+                return new Promise((resolve, reject) => {
+                    oModel.create("/UX_C_Booking_TP", oEntry, {
+                        success: function (oData, response) {
+                            resolve(oData.Bookid, response);
+                        },
+                        error: function (oError) {
+                            reject(oError);
+                        }
+                    });
+                });
+            },
+
+            _handleBookingSuccess: function (sBookid, oResponse) {
+                MessageBox.alert("Flight booked. Booking reference number: " + sBookid );
+            },
+
+            _handleBookingError: function (oError) {
+                if (oError) {
+                    if (oError.responseText) {
+                        var oErrorResponse = JSON.parse(oError.responseText);
+                        MessageBox.error("Error booking flight: " + oErrorResponse.error.message.value);
+                    }
+                }
+            }
+        }
     );
 });
